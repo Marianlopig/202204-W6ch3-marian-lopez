e28d0a1586ebe0dc385deb2d03198bcf74cd6e67
Marian <marianlopig@gmail.com>
2022-05-11 20:26:35 +0200

    adding the dispatch in keyboard

--- src/redux/features/phone/phoneSlice.test.js
+++ src/redux/features/phone/phoneSlice.test.js
@@ -4,13 +4,13 @@ describe("Given an slice with an addNumber reducer", () => {
   describe("When we pass a new number, 6", () => {
     test("Then it should return the actual number with a new 6 number added", () => {
       const actionType = "phone/addNumber";
-      const newNumber = 6;
+      const payload = 6;
 
       const expectedResult = 6;
 
-      const expectedReducer = phoneReducer(newNumber, actionType);
+      const numberAdded = phoneReducer(payload, actionType);
 
-      expect(expectedReducer).toBe(expectedResult);
+      expect(numberAdded).toBe(expectedResult);
     });
   });
 });
